63a2f94d4a949733a1073cc1a05c057fc16815a0
aaronboch <aaronboch8@gmail.com>
2026-06-04 01:01:07 +0200

    phong reflection model on triangle normals via fragment shader

--- main.cpp
+++ main.cpp
@@ -3,18 +3,22 @@
 #include "our_gl.hpp"
 #include "tgaimage.hpp"
 
+#include <cmath>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 
 extern mat4 ModelView, Viewport, Perspective;
 
-struct RandomShader : IShader {
+struct PhongShader : IShader {
     const Model& model;
     TGAColor color{};
+    vec3 l{};
     vec3 tri[3]; // tri in eye coordinates
 
-    RandomShader(const Model& m) : model(m) {}
+    PhongShader(const Model& m, const vec3 light) : model(m) {
+        l = (ModelView * vec4{light.x(), light.y(), light.z(), 0.}).xyz().norm();
+    }
 
     virtual vec4 vertex(int face, int vert) {
         vec3 v = model.vert(face, vert);
@@ -23,7 +27,37 @@ struct RandomShader : IShader {
         return Perspective * gl_Position;                             // in clip coords
     }
     virtual std::pair<bool, TGAColor> fragment(const vec3 bar) const {
-        return {false, color}; // no discard
+        double e = 35.;      // shininess exponent
+        double ambient = .3; // ambient light intensity
+
+        // calculate triangle normal vector
+        auto n = (tri[1] - tri[0]).cross(tri[2] - tri[0]).norm();
+        auto r = (2 * n * (n.dot(l)) - l).norm(); // reflection vec
+
+        // diffuse light intensity
+        auto diff = std::max(0., n.dot(l));
+        // spec light intensity
+
+        vec3 eye_pos{0., 0., -1. / Perspective[3][2]};
+        auto P = (tri[0] * bar.x() + tri[1] * bar.y() + tri[2] * bar.z());
+        auto v = (eye_pos - P).norm();
+
+        auto spec =
+            std::pow((std::max(0., r.dot(v))), e); // since camera is on z r.z would be sufficient
+
+        vec3 base{static_cast<double>(color.bgra[0] / 255.),
+                  static_cast<double>(color.bgra[1] / 255.),
+                  static_cast<double>(color.bgra[2] / 255.)};
+
+        vec3 color_f = base * (ambient + diff) + (vec3{1, 1, 1} * (spec));
+
+        color_f =
+            vec3{std::min(1., color_f.x()), std::min(1., color_f.y()), std::min(1., color_f.z())};
+
+        return {false,
+                TGAColor{{static_cast<uint8_t>((color_f.x() * 255)),
+                          static_cast<uint8_t>((color_f.y() * 255)),
+                          static_cast<uint8_t>((color_f.z() * 255))}}}; // no discard
     }
 };
 
@@ -35,6 +69,7 @@ int main(int argc, char** argv) {
 
     constexpr int width = 800; // output image size
     constexpr int height = 800;
+    constexpr vec3 light{1, 1, 1};  // unit length direction to sun
     constexpr vec3 eye{-1, 0, 2};   // camera position
     constexpr vec3 center{0, 0, 0}; // camera direction
     constexpr vec3 up{0, 1, 0};     // camera up vector
@@ -48,11 +83,11 @@ int main(int argc, char** argv) {
 
     for (int m = 1; m < argc; m++) {
         Model model{argv[m]};
-        RandomShader shader(model);
+        PhongShader shader(model, light);
         for (size_t f = 0; f < model.nfaces(); f++) {
-            shader.color = {{static_cast<uint8_t>(std::rand() % 255),
-                             static_cast<uint8_t>(std::rand() % 255),
-                             static_cast<uint8_t>(std::rand() % 255),
+            shader.color = {{static_cast<uint8_t>(128),
+                             static_cast<uint8_t>(128),
+                             static_cast<uint8_t>(128),
                              255}};
             Triangle clip = {shader.vertex(f, 0), shader.vertex(f, 1), shader.vertex(f, 2)};
             rasterize(clip, shader, framebuffer);
